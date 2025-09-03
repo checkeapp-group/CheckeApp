c0e1ad8ebef7b4bcdf397055bed1e09e6342d11e
Abraham <abrahammordev@gmail.com>
2025-09-03 10:31:08 +0200

    feat: remove files and update routes

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -1,41 +1,18 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import "../index.css";
-import Providers from "@/components/providers";
-import Header from "@/components/header";
+import RootLayout from '@/components/RootLayout';
+import I18nProvider from '@/providers/I18nProvider';
 
-const geistSans = Geist({
-	variable: "--font-geist-sans",
-	subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-	variable: "--font-geist-mono",
-	subsets: ["latin"],
-});
-
-export const metadata: Metadata = {
-	title: "FactCheckerProject",
-	description: "FactCheckerProject",
+type RootLayoutProps = {
+  children: React.ReactNode;
 };
 
-export default function RootLayout({
-	children,
-}: Readonly<{
-	children: React.ReactNode;
-}>) {
-	return (
-		<html lang="en" suppressHydrationWarning>
-			<body
-				className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-			>
-				<Providers>
-					<div className="grid grid-rows-[auto_1fr] h-svh">
-						<Header />
-						{children}
-					</div>
-				</Providers>
-			</body>
-		</html>
-	);
+export default function Layout({ children }: RootLayoutProps) {
+  return (
+    <html lang="es">
+      <body>
+        <I18nProvider>
+          <RootLayout>{children}</RootLayout>
+        </I18nProvider>
+      </body>
+    </html>
+  );
 }
