842a80432924d2867b0b13dd5d52c90bc446fa43
Daniel Cazalla (ZallaxDev) <86362063+ZallaxDev@users.noreply.github.com>
2025-11-04 01:53:00 +0100

    feat: google analytics integration

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import '@/../index.css';
 import Providers from '@/components/providers';
 import { I18nProvider } from '@/providers/I18nProvider';
+import Script from "next/script";
 
 type RootLayoutProps = {
   children: React.ReactNode;
@@ -10,6 +11,19 @@ export default function Layout({ children }: RootLayoutProps) {
   return (
     <html lang="es" suppressHydrationWarning>
       <body>
+      <Script
+          src={`https://www.googletagmanager.com/gtag/js?id=G-1MKPE3Q8TV`}
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-1MKPE3Q8TV');
+          `}
+        </Script>
+
         <I18nProvider>
           <Providers>{children}</Providers>
         </I18nProvider>
